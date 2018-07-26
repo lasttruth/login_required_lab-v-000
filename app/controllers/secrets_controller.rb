a7c653f1074133bac9c740ce228a886144c7f980
Robin Doirin <robindoirin@gmail.com>
2018-07-26 17:28:41 +0000

    automatically backed up by learn

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,4 @@
+class SecretsController < ApplicationController
+  def welcome
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'secrets/welcome'
+
   root 'sessions#new'
   get '/show' => 'secrets#show'
   get '/welcome' => 'secrets#welcome'
