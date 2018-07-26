401c8e6c04d849d7917694072a9f3bee9cf978a3
Robin Doirin <robindoirin@gmail.com>
2018-07-26 17:34:30 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,11 @@
 class SecretsController < ApplicationController
-  before_action :require_login, only: [:index, :login]
+  helper_method :current_user
+
   def show
+    if current_user == nil
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      @secret = session[:name]
+    end
   end
 end
