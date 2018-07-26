30071d2d7f87d63e79bf4fe812dacddf06ec720d
Robin Doirin <robindoirin@gmail.com>
2018-07-26 17:27:16 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,11 +13,11 @@ class SessionsController < ApplicationController
         else
           redirect_to welcome_path
         end
-
-        def destroy
-          session.delete(:name)
-          redirect_to root_path
-        end
       end
+
+  def destroy
+    session.delete(:name)
+    redirect_to root_path
+  end
 
 end
